cf00e6162e30a5388b5a5bd89ab1dc7ae3636bb9
aplactuan <aplactuan@gmail.com>
2023-05-02 00:16:23 +0800

    Just testing the script setup for the composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+  const message = ref("Hello World")
+
+  setTimeout(() => {
+    message.value = "Hello universe"
+  }, 2000)
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    {{ message }}
+    <input type="text" v-model="message">
   </main>
 </template>
